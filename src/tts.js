bc1537cba8536b67ddfd3e8fd8714fa7e01d630f
Vincent Lee <vv314@foxmail.com>
2020-03-02 00:47:11 +0800

    fix: 兼容 number 数据

--- src/tts.js
+++ src/tts.js
@@ -1,12 +1,16 @@
 const invoke = require('./lib/invoke')
+const { parseJson } = require('./lib/utils')
 
-function tts(ticket, text = '') {
-  return invoke({
+async function tts(ticket, text = '') {
+  const data = await invoke({
     method: 'text_to_speech',
-    message: { text },
+    // 确保 text 为 string
+    message: { text: `${text}` },
     path: 'mibrain',
     ticket
   })
+
+  return parseJson(data.info)
 }
 
 module.exports = tts
