08c0edd435ec2879fddb16004445de1110e2a5d5
Vincent Lee <vv314@foxmail.com>
2020-03-05 02:33:53 +0800

    chore: 查询指定音乐信息

--- src/player/getSongInfo.js
+++ src/player/getSongInfo.js
@@ -1,16 +1,7 @@
 const invoke = require('../lib/invoke')
-const { getStatus } = require('./getStatus')
 const { API } = require('../const')
 
 async function getSongInfo(ticket, songId) {
-  if (!songId) {
-    const status = await getStatus(ticket)
-
-    songId = status.play_song_detail.global_id
-
-    if (!songId) return null
-  }
-
   return await invoke({
     cookie: ticket.cookie,
     url: API.SONG_INFO,
