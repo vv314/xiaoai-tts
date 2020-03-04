94617dc93af5128c6902acce87c5eda22a3fffcd
Vincent Lee <vv314@foxmail.com>
2020-03-05 02:30:25 +0800

    chore: 添加 save 参数

--- src/tts.js
+++ src/tts.js
@@ -1,11 +1,11 @@
 const { ubus } = require('./lib/invoke')
-const { parseJson } = require('./lib/utils')
+const parseJson = require('./lib/parseBigIntJson')
 
 async function tts(ticket, text = '') {
   const data = await ubus(ticket, {
     method: 'text_to_speech',
     // 确保 text 为 string
-    message: { text: `${text}` },
+    message: { text: `${text}`, save: 0 },
     path: 'mibrain'
   })
 
